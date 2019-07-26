e87aa65f618d40e05a78f989d8a4934f73a01d39
zjade <zjade@outlook.com>
2019-07-26 10:38:58 +0800

    TravisCI swift_version Tests

---
+++ SwifterRouterTests/SwifterRouterTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwifterRouterTests.swift
+//  SwifterRouterTests
+//
+//  Created by Apple on 2019/7/26.
+//  Copyright © 2019 zjade. All rights reserved.
+//
+
+import XCTest
+
+class SwifterRouterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
